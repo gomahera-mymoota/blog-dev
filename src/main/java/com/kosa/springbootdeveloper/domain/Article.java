f1d1e1b23f1458469c0a352167baae6f7adff616
gomahera-mymoota <iceman4u@naver.com>
2023-06-29 14:29:15 +0900

    feat: Start blog project

---
+++ src/main/java/com/kosa/springbootdeveloper/domain/Article.java
@@ -0,0 +1,30 @@
+package com.kosa.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
