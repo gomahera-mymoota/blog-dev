8ac8d812bb9a9759a4c1915f39244789a6d760f6
IceMan4U <iceman4u@naver.com>
2023-06-30 01:55:42 +0900

    feat: 블로그 글 수정

--- src/main/java/com/kosa/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/kosa/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -35,5 +36,17 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponseDto(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String createArticle(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponseDto());
+        } else {
+            Article article = articleService.findById(id);
+            model.addAttribute("article", new ArticleViewResponseDto(article));
+        }
+
+        return "newArticle";
     }
 }
